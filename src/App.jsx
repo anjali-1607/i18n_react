dd3192bc7570c52d89947dcf887cdb485bde7b3c
anjali-1607 <anjalirajpoot1609@gmail.com>
2024-06-22 12:34:43 +0530

    successfully added i18n

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
 import React from "react";
 import "./App.css";
+import { useTranslation } from "react-i18next";
 
 export default function App() {
-    return <div>App</div>;
+    const { t, i18n } = useTranslation();
+
+    const languages = [
+        { code: "en", name: "English" },
+        { code: "de", name: "German" },
+    ];
+    return (
+        <>
+            <div>
+                <h1>{t("welcome")} Monster...</h1>
+                {languages.map((language) => (
+                    <button
+                        style={{ marginRight: "1rem" }}
+                        onClick={() => i18n.changeLanguage(language.code)}
+                        key={language.code}>
+                        {language.name}
+                    </button>
+                ))}
+            </div>
+        </>
+    );
 }
--- src/i18n.jsx
+++ src/i18n.jsx
@@ -1,5 +1,24 @@
-import React from "react";
+import { initReactI18next } from "react-i18next";
+import i18n from "i18next";
 
-export default function i18n() {
-    return <div>i18n</div>;
-}
+i18n.use(initReactI18next).init({
+    debug: true,
+    fallbackLng: "en",
+    interpolation: {
+        escapeValue: false,
+    },
+    resources: {
+        en: {
+            translation: {
+                welcome: "Welcome",
+            },
+        },
+        de: {
+            translation: {
+                welcome: "Willkommen",
+            },
+        },
+    },
+});
+
+export default i18n;
